64527c3050bc9a4edf461c9d7d3e2c2911aa4d67
milo720 <milo_farrell@yahoo.co.uk>
2025-01-27 14:32:20 +0000

    Add vector search

--- src/ContosoSuitesDashboard/pages/3_Vector_Search.py
+++ src/ContosoSuitesDashboard/pages/3_Vector_Search.py
@@ -47,15 +47,11 @@ def main():
     if st.button("Submit"):
         with st.spinner("Performing vector search..."):
             if query:
-                # Vectorize the query text.
-                # Exercise 3 Task 3 TODO #4: Get the vectorized query text by calling handle_query_vectorization.
-                
-                # Perform the vector search.
-                # Exercise 3 Task 3 TODO #5: Get the vector search results by calling handle_vector_search.
-                
-                # Display the results.
-                st.write("## Results")
-                # Exercise 3 Task 3 TODO #6: Display the results as a table.
+
+                   query_vector = handle_query_vectorization(query)
+                   vector_search_results = handle_vector_search(query_vector, max_results, minimum_similarity_score)
+                   st.write("## Results")
+                   st.table(vector_search_results.json())
                 
             else:
                 st.warning("Please enter a query.")
